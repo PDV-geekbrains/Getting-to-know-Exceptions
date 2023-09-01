a751418ffabdbcf785979ad7f7556c0b7954b4c0
Dmitriy <pd2018@yandex.ru>
2023-09-01 14:57:30 +0300

    Added test_01

---
+++ class_02/test_01/program.java
@@ -0,0 +1,30 @@
+package test_01;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        float userInput = getUserInput();
+        System.out.println(userInput);
+    }
+
+    private static float getUserInput() {
+        boolean isValid = false;
+        Scanner scanner = new Scanner(System.in);
+        float result = 0;
+        while (!isValid) {
+            try {
+                System.out.print("Введите дробное число: ");
+                result = scanner.nextFloat();
+                isValid = true;
+            } catch (InputMismatchException e) {
+                isValid = false;
+                System.out.println("Ошибка. Введён неправильный формат данных.");
+                scanner.nextLine();
+            }
+        }
+        scanner.close();
+        return result;
+    }
+}
